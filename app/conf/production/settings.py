07c8f40a8ec946cce3fded66c6bfc7adb368bb8c
FernandoCelmer <email@fernandocelmer.com>
2021-10-18 00:38:54 -0300

    Atualizaçao configuraçoes de Ambiente

--- app/conf/production/settings.py
+++ app/conf/production/settings.py
@@ -8,9 +8,7 @@ BASE_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
 CONTENT_DIR = os.path.join(BASE_DIR, 'content')
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = [
     'upy-application.herokuapp.com',
 ]
