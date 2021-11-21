e35224b88b0a9925c8ac8558db8a275a21ab7929
FernandoCelmer <email@fernandocelmer.com>
2021-11-21 02:24:40 -0300

    UPY-#69 - Add global_permissions in ChangeLanguageView

--- modules/main/views.py
+++ modules/main/views.py
@@ -1,9 +1,18 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 
 class IndexPageView(TemplateView):
     template_name = 'main/index.html'
 
 
-class ChangeLanguageView(TemplateView):
+class ChangeLanguageView(LoginRequiredMixin, TemplateView):
     template_name = 'main/change_language.html'
+
+    def get(self, request, *args, **kwargs):
+        if not self.request.user.has_perm('global_permissions.language_view'):
+            raise PermissionDenied
+
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
