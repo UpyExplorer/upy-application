d161ae29020de7337de301c368fcb9152b446eef
FernandoCelmer <email@fernandocelmer.com>
2021-11-21 21:05:28 -0300

    No Task - Change module

--- modules/base/models.py
+++ modules/base/models.py
@@ -1,14 +1,21 @@
+
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import Group
 
 
 class Configuration(models.Model):
-    code = models.CharField(max_length=20, unique=True)
+    key = models.CharField(max_length=50,blank=True, null=True)
+    description = models.CharField(max_length=50,blank=True, null=True)
+    value = models.CharField(max_length=10,blank=True, null=True)
 
 class Integration(models.Model):
     code = models.CharField(max_length=20, unique=True)
 
+
 class Module(models.Model):
     code = models.CharField(max_length=20, unique=True)
+
 
 class Plan(models.Model):
     code = models.CharField(max_length=20, unique=True)
