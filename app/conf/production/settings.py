50ec759620300d1e1f72306c9dee5f5e461a98fa
FernandoCelmer <email@fernandocelmer.com>
2021-11-23 01:31:05 -0300

    No Task - Test change ALLOWED_HOSTS

--- app/conf/production/settings.py
+++ app/conf/production/settings.py
@@ -11,7 +11,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['upy-application.herokuapp.com']
 
 SITE_ID = 1
 DISABLE_COLLECTSTATIC = 1
