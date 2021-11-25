4f61b1513db1438e9c373378b9874e9fb737656e
FernandoCelmer <email@fernandocelmer.com>
2021-11-25 01:07:28 -0300

    No Task - Update Scripts

--- script_create_branch.py
+++ script_create_branch.py
@@ -4,6 +4,9 @@ print('List Branch: ')
 list_branch = input()
 os.system("git checkout develop")
 
+if type(list_branch) == str:
+    list_branch = list_branch.split(",")
+
 if list_branch:
     for branch in list_branch:
         os.system("git checkout -b UPY-#"+ str(branch))
