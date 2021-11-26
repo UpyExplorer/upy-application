e487ffa86257753f83b6e9e06691273e3a97e8ab
FernandoCelmer <email@fernandocelmer.com>
2021-11-26 02:36:28 -0300

    UPY-#94 - Change name model

--- modules/order/models.py
+++ modules/order/models.py
@@ -4,6 +4,6 @@ from django.db import models
 from modules.company.models import Data
 
 
-class Info(models.Model):
+class Data(models.Model):
     company_data = models.ForeignKey(Data, on_delete=models.SET_NULL, null=True)
     created_on = models.DateTimeField(default=datetime.now, null=True)
