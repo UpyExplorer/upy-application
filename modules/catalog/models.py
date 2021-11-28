7b1f11e1af39e8b687bb9d23deaf3abdc2d7a30f
FernandoCelmer <email@fernandocelmer.com>
2021-11-28 00:12:08 -0300

    UPY-#95 - Create module catalog and table catalog_product

---
+++ modules/catalog/models.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime
+from django.db import models
+from modules.company.models import Data
+
+
+class Product(models.Model):
+    company_data = models.ForeignKey(Data, on_delete=models.SET_NULL, null=True)
+    creation_time = models.DateTimeField(default=datetime.now, null=True)
+    name = models.CharField(max_length=100, blank=False, null=False, default='Product')
+    description = models.TextField(blank=True, null=True)
+    external_code = models.CharField(max_length=25, blank=False, null=True)
+    code_ean_gtin = models.CharField(max_length=25, blank=False, null=True)
+    code = models.CharField(max_length=25, blank=False, null=True)
+    sku = models.CharField(max_length=25, blank=False, null=True)
+    price_sell = models.DecimalField(max_digits=11, decimal_places=7, null=True)
+    price_cost = models.DecimalField(max_digits=11, decimal_places=7, null=True)
+    price_promo = models.DecimalField(max_digits=11, decimal_places=7, null=True)
+    detail_weight = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    detail_length = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    detail_width = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    detail_height = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    active = models.BooleanField(null=False, default=False)
+    video_url = models.TextField(blank=True, null=True)
