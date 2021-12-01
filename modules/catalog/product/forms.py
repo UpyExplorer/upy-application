c2852f198d5a0a66f76a653461f16dba90dd2527
FernandoCelmer <email@fernandocelmer.com>
2021-12-01 19:55:23 -0300

    UPY-#119 - Implementation initial for edit product

--- modules/catalog/product/forms.py
+++ modules/catalog/product/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from modules.catalog.product.models import Product
 
-class ProductForm(forms.ModelForm):
-    name = forms.CharField(help_text="Product name")
-    sku = forms.CharField(help_text="Sku")
-
-    class Meta:
-        model = Product
-        exclude = ['creation_time', 'company_data', 'video_url']
-
-# class ProductForm(forms.Form):
+# class ProductForm(forms.ModelForm):
 #     name = forms.CharField(help_text="Product name")
 #     sku = forms.CharField(help_text="Sku")
+
+#     class Meta:
+#         model = Product
+#         exclude = ['creation_time', 'company_data', 'video_url']
+
+class ProductForm(forms.Form):
+    name = forms.CharField(help_text="Product name")
+    sku = forms.CharField(help_text="Sku")
 
