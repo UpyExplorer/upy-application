7b837236d53d0b8bad92f4ca42a951446ef7ee66
FernandoCelmer <email@fernandocelmer.com>
2021-12-03 20:49:41 -0300

    UPY-#127 - Product form adjustment

--- modules/catalog/product/forms.py
+++ modules/catalog/product/forms.py
@@ -3,10 +3,11 @@ from django.utils.translation import gettext_lazy as _
 from modules.catalog.product.models import Product
 
 class ProductForm(forms.ModelForm):
+    active = forms.BooleanField(label=_("Active"))
     name = forms.CharField(label=_("Nome"), help_text=_("Maximum 100 characters"))
     sku = forms.CharField(label=_("Sku"), help_text=_("No special characters allowed"))
     type = forms.ChoiceField(label=_("Tipo"), choices=Product.ITEM_TYPE)
-    stock = forms.DecimalField(label=_("Estoque"))
+    stock = forms.IntegerField(label=_("Estoque"))
     price_sell = forms.DecimalField(label=_("Preço de Venda"))
     price_cost = forms.DecimalField(label=_("Preço de Custo"))
     price_promo = forms.DecimalField(label=_("Preço Promocional"))
@@ -14,6 +15,6 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['name', 'sku', 'type', 'stock', 'price_sell', 'price_cost', 'price_promo', 'description']
+        fields = ['active','name', 'sku', 'type', 'stock', 'price_sell', 'price_cost', 'price_promo', 'description']
         exclude = ['creation_time', 'company_data', 'video_url']
 
