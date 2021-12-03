f3dba57ba2e214fd0e5b94fe51627f1710f4ca7f
FernandoCelmer <email@fernandocelmer.com>
2021-12-03 03:00:30 -0300

    UPY-#119 - Add labels

--- modules/catalog/product/forms.py
+++ modules/catalog/product/forms.py
@@ -3,11 +3,14 @@ from django.utils.translation import gettext_lazy as _
 from modules.catalog.product.models import Product
 
 class ProductForm(forms.ModelForm):
-    name = forms.CharField(help_text=_("Maximum 100 characters"))
-    sku = forms.CharField(help_text=_("No special characters allowed"))
-    price_sell = forms.DecimalField()
-    price_cost = forms.DecimalField()
-    price_promo = forms.DecimalField()
+    name = forms.CharField(label=_("Nome"), help_text=_("Maximum 100 characters"))
+    sku = forms.CharField(label=_("Sku"), help_text=_("No special characters allowed"))
+    type = forms.CharField(label=_("Tipo"))
+    stock = forms.DecimalField(label=_("Estoque"))
+    price_sell = forms.DecimalField(label=_("Preço de Venda"))
+    price_cost = forms.DecimalField(label=_("Preço de Custo"))
+    price_promo = forms.DecimalField(label=_("Preço Promocional"))
+    description = forms.CharField(label=_("Descrição"))
 
     class Meta:
         model = Product
