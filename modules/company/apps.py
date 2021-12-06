9df226adf5729f476f640939def7ee5fe4db56c9
FernandoCelmer <email@fernandocelmer.com>
2021-12-06 00:01:59 -0300

    UPY-#152 - Include verbose_name

--- modules/company/apps.py
+++ modules/company/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CompanyConfig(AppConfig):
     name = 'modules.company'
+    verbose_name = 'Upy-Company'
--- modules/log/apps.py
+++ modules/log/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class LogConfig(AppConfig):
     name = 'modules.log'
+    verbose_name = 'Upy-Log'
