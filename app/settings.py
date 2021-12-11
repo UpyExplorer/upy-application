0964dbb590e9f0eb8a839acdf90aa6c4ccf9edcd
FernandoCelmer <email@fernandocelmer.com>
2021-12-11 00:27:35 -0300

    UPY-#167 - Update settings

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,4 @@ import os
 
 DJANGO_ENV = os.environ.get('DJANGO_ENV')
 
-if DJANGO_ENV == 'production':
-    from .conf.production.settings import *
-else:
-    from .conf.development.settings import *
+from .conf.production.settings import *
