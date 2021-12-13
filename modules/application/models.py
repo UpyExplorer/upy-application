c0649a5578f055fb9ed814b7aa454c8e8b85e29e
FernandoCelmer <email@fernandocelmer.com>
2021-12-13 04:29:57 -0300

    UPY-#102 - Create module Application

---
+++ modules/application/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from modules.base.models import ModelUpyBase
+
+class BaseApplication(ModelUpyBase):
+    name = models.CharField(max_length=100, blank=False, null=False, default='Application')
+    code = models.CharField(max_length=25, blank=False, null=True)
+    key = models.CharField(max_length=50,blank=True, null=True)
+    description = models.CharField(max_length=50,blank=True, null=True)
+    url = models.CharField(max_length=100,blank=True, null=True)
+
+    class Meta:
+        db_table = 'base_application'
