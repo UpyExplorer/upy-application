405fe23fee9a77db625880e0812fb63dafe57864
FernandoCelmer <email@fernandocelmer.com>
2021-12-23 00:14:39 -0300

    UPY-#211 - Create options products name

--- modules/ads/forms.py
+++ modules/ads/forms.py
@@ -7,9 +7,15 @@ from django.utils.translation import gettext_lazy as _
 from modules.catalog.product.models import Product
 from modules.ads.models import AdsLink
 
+from django.forms import ModelChoiceField
+
+class ProductChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s" % obj.name
+
 
 class AdsLinkForm(forms.ModelForm):
-    product = forms.ModelChoiceField(
+    product = ProductChoiceField(
         widget=forms.Select,
         queryset=Product.objects.all()
         )
