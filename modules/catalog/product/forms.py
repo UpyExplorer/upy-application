194c24f719fd48a87f18d704bcb14465e13ad75f
FernandoCelmer <email@fernandocelmer.com>
2021-12-26 00:09:51 -0300

    UPY-#217 - Add field for categories in products

--- modules/catalog/product/forms.py
+++ modules/catalog/product/forms.py
@@ -1,8 +1,19 @@
 
 from decimal import Decimal
+
 from django import forms
 from django.utils.translation import gettext_lazy as _
+
+from app.base import BaseChoiceField
+
+from modules.catalog.category.models import Category
 from modules.catalog.product.models import Product
+
+
+class ChoiceFieldCategory(BaseChoiceField):
+    def label_from_instance(self, obj):
+        return "%s" % obj.name
+
 
 class ProductForm(forms.ModelForm):
 
@@ -12,10 +23,22 @@ class ProductForm(forms.ModelForm):
                 kwargs['instance'].price_sell = kwargs['instance'].price_sell.quantize(Decimal('0.00'))
                 kwargs['instance'].price_cost = kwargs['instance'].price_cost.quantize(Decimal('0.00'))
                 kwargs['instance'].price_promo = kwargs['instance'].price_promo.quantize(Decimal('0.00'))
+
+                company_data_id = kwargs['instance'].company_data_id
+
+                category = Category.objects.filter(
+                    company_data=company_data_id
+                    ).all()
+
         super(ProductForm, self).__init__(*args, **kwargs) 
         self.fields['price_sell'].decimal_places = 2
         self.fields['price_cost'].decimal_places = 2
         self.fields['price_promo'].decimal_places = 2
+        self.fields['category'] = ChoiceFieldCategory(
+            widget=forms.Select,
+            queryset=category,
+            label=_("Category")
+            )
 
     active = forms.BooleanField(label=_("Active"), required=False)
     name = forms.CharField(label=_("Name"), help_text=_("Maximum 100 characters"))
@@ -39,13 +62,14 @@ class ProductForm(forms.ModelForm):
         fields = [
             'active',
             'name',
+            'category',
             'sku',
             'type',
             'condition',
             'stock',
             'price_sell',
-            'price_cost',
             'price_promo',
+            'price_cost',
             'description',
         ]
         exclude = ['creation_time', 'company_data', 'video_url']
